5a572c685ebf1d0356dc6e374233fbce2be5ecfe
crestharpan <arpanshrestha225@gmail.com>
2025-04-25 13:50:04 +0545

    crud for blood donors

--- Routes/bloodDonorRouter.js
+++ Routes/bloodDonorRouter.js
@@ -3,4 +3,10 @@ const bloodDonorController = require('../Controllers/bloodDonorController');
 const router = express.Router();
 
 router.post('/', bloodDonorController.add);
+router.get('/create', bloodDonorController.getAll);
+router
+  .route('/:id')
+  .get(bloodDonorController.getOne)
+  .patch(bloodDonorController.update)
+  .delete(bloodDonorController.delete);
 module.exports = router;
