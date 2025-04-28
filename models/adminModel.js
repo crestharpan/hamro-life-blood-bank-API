9080de14ff59ad50409b6adf0eb25b3e9f4b7ce9
crestharpan <arpanshrestha225@gmail.com>
2025-04-28 21:15:17 +0545

    implemented the password hashing with bcrypt

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = mongoose.Schema({
   fullName: {
@@ -19,6 +20,16 @@ const adminSchema = mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+});
+
+adminSchema.pre('save', async function (next) {
+  //IF PASSWORD IS NOT MODIFIED RETURN DON NOT EXECUTE THIS MIDDLEWARE
+  if (!this.isModified('password')) return;
+
+  //HAS THE PASSWORD BEFORE SAVING TO THE DB
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
 });
 
 const admin = mongoose.model('admin', adminSchema);
