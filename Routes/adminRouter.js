641fca60465cf1debdd6138daf652e67cd3ad226
crestharpan <arpanshrestha225@gmail.com>
2025-04-30 21:55:23 +0545

    Refactoring the bloodReuests routes

--- Routes/adminRouter.js
+++ Routes/adminRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const bloodBankController = require('../Controllers/bloodBankController');
+
 const authController = require('../Controllers/authController');
 
 const router = express.Router();
@@ -7,5 +9,19 @@ const router = express.Router();
 router.post('/signup', authController.adminSignup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+
+router
+  .route('/bloodrequests')
+  .post(bloodBankController.add)
+  .get(bloodBankController.getAll);
+
+router
+  .route('/bloodrequests/:id')
+  .get(bloodBankController.getOne)
+  .patch(bloodBankController.update)
+  .delete(bloodBankController.delete);
+router.post('/bloodBank', bloodBankRouter);
+router.post('/bloodDonor', bloodDonorRouter);
+router.post('/getCities', userRequestRouter);
 
 module.exports = router;
