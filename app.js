6b025b3c71f590703c531a7ca6826941dc612dbf
crestharpan <arpanshrestha225@gmail.com>
2025-05-02 22:07:19 +0545

    added security

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoSantitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const adminRouter = require('./Routes/adminRouter');
 
 const cors = require('cors');
@@ -10,6 +12,12 @@ const cors = require('cors');
 app.use(bodyParser.json());
 //IT WILL RETURN REQ HEADER
 app.use(morgan('dev'));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSantitize());
+
+//DATA SANTIZATION AGAINST CROSS-SITE ATTACK
+app.use(xss());
 
 //ROUTE FOR THE ADMIN
 app.use('/api/V1/admin', adminRouter);
