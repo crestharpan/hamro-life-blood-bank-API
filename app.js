9f8ebb6b3e74c01d71846fc31346c0e4f50d7855
crestharpan <arpanshrestha225@gmail.com>
2025-05-13 22:47:05 +0545

    cors configuration

--- app.js
+++ app.js
@@ -9,15 +9,22 @@ const adminRouter = require('./Routes/adminRouter');
 
 const cors = require('cors');
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // if you're using cookies or sessions
+  })
+);
+
 app.use(bodyParser.json());
 //IT WILL RETURN REQ HEADER
 app.use(morgan('dev'));
 
 //DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
-app.use(mongoSantitize());
+// app.use(mongoSantitize());
 
-//DATA SANTIZATION AGAINST CROSS-SITE ATTACK
-app.use(xss());
+// //DATA SANTIZATION AGAINST CROSS-SITE ATTACK
+// app.use(xss());
 
 //ROUTE FOR THE ADMIN
 app.use('/api/V1/admin', adminRouter);
